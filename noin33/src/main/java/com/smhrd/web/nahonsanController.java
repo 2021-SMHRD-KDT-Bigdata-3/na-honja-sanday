4203a0e8946262a0b349aa2a21a3f25986ab9eac
smhrd <smhrd@DESKTOP-SQCQT3L>
2021-07-31 17:24:25 +0900

    commit

--- noin33/src/main/java/com/smhrd/web/nahonsanController.java
+++ noin33/src/main/java/com/smhrd/web/nahonsanController.java
@@ -12,36 +12,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+import com.smhrd.mapper.guardianVO;
 import com.smhrd.mapper.nahonsanMapper;
 import com.smhrd.mapper.testVO;
 
 @Controller
 public class nahonsanController {
 
-	@Autowired
-	nahonsanMapper naMapper;
-	
-	
-	@RequestMapping("/firstpage")
-	public String first() {
-		return "firstpage";
-	}
-	
-	
-	/*
-	 * @RequestMapping("/loginTEST.do") public String loginTEST(testVO test) {
-	 * testVO vo = naMapper.login(test);
-	 * 
-	 * if(vo==null) { System.out.println("로그인 실패"); return "logintest"; } else {
-	 * return "loginsuccesstest"; }
-	 * 
-	 * }
-	 */
-	
 
    private static final Logger logger = LoggerFactory.getLogger(nahonsanController.class);
    
+   @Autowired
+   nahonsanMapper naMapper;
  
    @RequestMapping(value = "/", method = RequestMethod.GET)
    public String home(Locale locale, Model model) {
@@ -56,36 +38,102 @@ public class nahonsanController {
       
       return "Home";
    }
-   @RequestMapping(value = "/index.do")
-   public String home() {
-      return "index";
+   
+   // 만들어지지 않은 페이지
+   //@RequestMapping("/firstpage")
+	//public String first() {
+		//return "firstpage";
+	//}
+
+   /*
+	 * @RequestMapping("/loginTEST.do") public String loginTEST(testVO test) {
+	 * testVO vo = naMapper.login(test);
+	 * 
+	 * if(vo==null) { System.out.println("로그인 실패"); return "logintest"; } else {
+	 * return "loginsuccesstest"; }
+	 * 
+	 * }
+	 */
+	
+   
+   
+   @RequestMapping(value = "/login.do")
+   public String login(guardianVO guardian) {
+	   guardianVO guard = naMapper.login(guardian);
+	   
+	   if(guard == null) {
+		   return "login";
+	   }else {
+		   return "main_guard";
+	   }
    }
-   @RequestMapping("/About.do")
+  
+   @RequestMapping(value = "/About.do")
    public String about() {
       return "about";
    }
-   @RequestMapping(value = "/blog.do")
-   public String Home() {
+   
+   @RequestMapping(value = "/join.do")
+   public String join() {
+      return "join";
+   }
+   @RequestMapping(value = "/apply.do")
+   public String apply() {
       return "blog";
    }
-   @RequestMapping(value = "/contract.do")
-   public String Contract() {
+   @RequestMapping(value = "/apply_complete.do")
+   public String apply_complete() {
       return "contract";
    }
-   @RequestMapping(value = "/blog-single.do")
-   public String blog_single() {
+   @RequestMapping(value = "/apply_status.do")
+   public String apply_status() {
       return "blog-single";
    }
-   @RequestMapping(value = "/counselor.do")
-   public String counselor() {
+   @RequestMapping(value = "/main_guard.do")
+   public String main_guard() {
       return "counselor";
    }
-   @RequestMapping(value = "/pricing.do")
-   public String pricing() {
+   @RequestMapping(value = "/main_senior.do")
+   public String main_senior() {
       return "pricing";
    }
-   @RequestMapping(value = "/services.do")
-   public String services() {
+   @RequestMapping(value = "/senior_msg.do")
+   public String senior_msg() {
       return "services";
    }
+   
+   
+	/*
+	 * jsp가 만들어지지 않은 페이지
+	 *
+	 * @RequestMapping("/senior_friend.do") 
+	 * public String seniorfriend() { 
+	 * return "senior_friend"; 
+	 * }
+	 * 
+	 * @RequestMapping("/wel_manage.do") 
+	 * public String manage() { 
+	 * return "wel_manage"; 
+	 * }
+	 * 
+	 * @RequestMapping("/danger.do") 
+	 * public String danger() {
+	 * return "danger"; 
+	 * }
+	 * 
+	 */
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
